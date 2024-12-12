35edfc1ca35779fa48291b81ce0a6969578608ab
Bentleylol86 <Laurence.Woodhouse@bentley.co.uk>
2024-12-12 12:30:39 +0000

    Exersise 2.1

--- exercises/validateId.js
+++ exercises/validateId.js
@@ -8,19 +8,32 @@ export function validateId(id) {
 
   // The id should be a string:
   if (typeof id !== 'string') {
-    throw new Error (`ID ins't a string`)
+    throw new Error (`ID isn't a string`)
     // TODO: Add your code to throw an error here
   }
 
+  if (id.length % 2 === 0) {
+    throw new Error (`ID isn't an odd number of caracter long`)
+  }
   // The id should be an odd number of characters long:
   // TODO: throw an error on this condition
+  if (!id.includes('a')){
 
+    throw new Error (`ID does not contain the letter a`)
+  }
   // The id should contain the letter 'a':
   // TODO: throw an error on this condition
+  if (id !== id.toLowerCase()){
 
+    throw new Error (`ID is not all lower case`);
+  }
+  
+
+    return  true; 
+  }
   // The id should be all lowercase:
   // TODO: throw an error on this condition
-}
+
 
 // Exercise 2 Part 2
 // Use try/catch with the `validateId` function above to validate the id.
